cc8957e7baeea5cad4deb344cb947d12f9d7c17b
thimgrim1337 <11806430+thimgrim1337@users.noreply.github.com>
2022-10-20 17:30:24 +0200

    Create class Player

---
+++ src/js/Player.js
@@ -0,0 +1,9 @@
+class Player {
+  constructor(playerChoice) {
+    this.playerChoice = playerChoice;
+  }
+  getPlayerChoice() {
+    return this.playerChoice;
+  }
+}
+export default Player;
--- src/js/app.js
+++ src/js/app.js
@@ -8,6 +8,10 @@ class Statistics - przechowuje rozegrane gry i zwraca statystyki
 */
 
 import AI from './AI.js';
+import Player from './Player.js';
 
 const ai = new AI(['rock', 'paper', 'scissors']);
 console.log(ai.makeChoice());
+
+const player = new Player('rock');
+console.log(player.getPlayerChoice());
