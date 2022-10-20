e1dd8b7d55036ff57020beb6dc584e1369d822f7
thimgrim1337 <11806430+thimgrim1337@users.noreply.github.com>
2022-10-20 11:30:51 +0200

    Parcel setup

---
+++ src/js/app.js
@@ -0,0 +1,7 @@
+/* 
+
+class Game - uruchamia grę buttonem
+class Round - pobiera wybór gracza, losuje wybór komputera 
+class Result - zwraca wynik gry
+class Statistics - przechowuje rozegrane gry i zwraca statystyki
+*/
