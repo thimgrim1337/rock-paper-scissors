8fda1e291af992703ef00083b519d9ac3e541d8f
thimgrim1337 <11806430+thimgrim1337@users.noreply.github.com>
2022-10-24 21:57:41 +0200

    Add and style UI

--- src/js/Game.js
+++ src/js/Game.js
@@ -5,13 +5,15 @@ import Stats from './Stats';
 
 class Game {
   constructor() {
-    this.options = document.querySelectorAll('[data-option]');
+    this.options = document.querySelectorAll('.choice img[data-option]');
     this.player = new Player();
     this.options.forEach((option) =>
       option.addEventListener('click', (e) => {
         this.player.setPlayerChoice(e.target.dataset.option);
-        this.options.forEach((option) => (option.style.border = ''));
-        e.target.style.border = '1px solid black';
+        this.options.forEach(
+          (option) => (option.style.boxShadow = '0 0 10px black')
+        );
+        e.target.style.boxShadow = '0 0 15px white';
       })
     );
     this.stats = new Stats();
@@ -30,19 +32,19 @@ class Game {
     this.spanResult.textContent = game.msg;
     this.spanTotal.textContent = stats[0];
     this.spanWins.textContent = stats[1];
-    this.spanDraws.textContent = stats[2];
-    this.spanLosses.textContent = stats[3];
+    this.spanLosses.textContent = stats[2];
+    this.spanDraws.textContent = stats[3];
   }
 
   startGame() {
     const ai = new AI(this.options);
-    const win = Result.checkWinner(
+    const game = Result.checkWinner(
       this.player.getPlayerChoice(),
       ai.getAIChoice()
     );
-    this.stats.addGameToStats(win.result);
+    this.stats.addGameToStats(game.result);
 
-    this.render(win, this.stats.showGameStats());
+    this.render(game, this.stats.showGameStats());
   }
 }
 export default Game;
--- src/js/Stats.js
+++ src/js/Stats.js
@@ -11,7 +11,7 @@ class Stats {
     let games = this.gameResult.length;
     let wins = this.gameResult.filter((result) => result === true).length;
     let losses = this.gameResult.filter((result) => result === false).length;
-    let draws = this.gameResult.filter((result) => result === 'remis').length;
+    let draws = this.gameResult.filter((result) => result === 'draw').length;
     return [games, wins, losses, draws];
   }
 }
